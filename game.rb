c8a2f6e9ce3007d536a7f14eef086f11ff86b768
Nikolai <nikolaigauer@gmail.com>
2017-08-01 04:14:52 +0000

    Initial commit

---
+++ game.rb
@@ -0,0 +1,17 @@
+# Player will keep track of life method wich will be initialized with 3/3 lives
+# will get instructions from Turn which turn it is
+class Player
+  life ()
+end
+
+# Game will hold the entirety together
+class Game
+end
+
+# Question will hold questions and get instructions from Turn when to fire questions
+class Question
+end
+
+# Turn will hold the turn logic
+class Turn
+end
