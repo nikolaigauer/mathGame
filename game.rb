9199b813ed9bafb984cba5ade07005f9110346c6
Nikolai <nikolaigauer@gmail.com>
2017-08-08 04:59:48 +0000

    Search for fix in game

--- game.rb
+++ game.rb
@@ -6,16 +6,16 @@ Class Game
       turn = Turn.new(player1, player2, @turns_left) 
       @turns_left = @turns_left - 1
     end
-    if(player1.lives > player2.lives)
-      winner = player1
-      loser = player2
-      puts "Game Over! Player #{loser.number} and Player #{winner.number} still has #{winner.lives} lives left."
-    elsif(player2.lives > player1.lives)
-      winner = player2
-      loser = player1
-      puts "Game Over! Player #{loser.number}  Player #{winner.number} still has #{winner.lives} lives left."
-    else
-      puts "Nobody won, exciting."
-    end  
+    # if(player1.lives > player2.lives)
+    #   winner = player1
+    #   loser = player2
+    #   puts "Game Over! Player #{loser.number} and Player #{winner.number} still has #{winner.lives} lives left."
+    # elsif(player2.lives > player1.lives)
+    #   winner = player2
+    #   loser = player1
+    #   puts "Game Over! Player #{loser.number}  Player #{winner.number} still has #{winner.lives} lives left."
+    # else
+    #   puts "Nobody won, exciting."
+    # end  
   end
 end
